38c48e6f55be17ac7b1810fdd3b520fa4c19aa4e
zzhu2011 <zzhu1987@gmail.com>
2015-03-17 22:08:10 -0700

    Less GC and More memory

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
- '(highlight ((t (:foreground "yellow"))))
+ '(highlight ((t (:background "#073642"))))
  '(linum-highlight-face ((t (:inherit default :foreground "yellow"))))
  '(show-paren-match ((t (:background "gray" :foreground "snow" :inverse-video nil :underline nil :slant normal :weight bold)))))
 (require 'package)
@@ -21,6 +21,17 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/")
              t)
+
+(setq ring-bell-function 'ignore)
+;;----------------------------------------------------------------------------
+;; Less GC, more memory
+;;----------------------------------------------------------------------------
+;; By default Emacs will initiate GC every 0.76 MB allocated
+;; (gc-cons-threshold == 800000).
+;; we increase this to 512MB
+;; @see http://www.gnu.org/software/emacs/manual/html_node/elisp/Garbage-Collection.html
+(setq-default gc-cons-threshold (* 1024 1024 512)
+              gc-cons-percentage 0.5)
 
 (setq package-enable-at-startup nil)
 (package-initialize)
